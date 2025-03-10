79b57980963cac2ef378d7e5ac79d67c4b225672
Victor Ramirez <victor.ramirez.c2010@gmail.com>
2025-03-10 17:06:55 -0500

    proyecto inicial con postgres

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Teslo Api
+
+
+1. Levantar la base de datos
+```
+docker-compose up -d
+```
---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  
+  imports: [ 
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,      
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+   ],
+})
+export class AppModule {}
